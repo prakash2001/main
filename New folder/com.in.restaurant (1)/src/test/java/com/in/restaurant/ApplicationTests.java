ff937baa90072e010638979933f3837894f1d3a6
prakash2001 <54575677+prakash2001@users.noreply.github.com>
2023-05-30 08:48:47 +0530

    Add files via upload

---
+++ New folder/com.in.restaurant (1)/src/test/java/com/in/restaurant/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in.restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ New folder/connect.jsp
@@ -0,0 +1,3 @@
+<%
+String firstname= repuesasd
+%>
